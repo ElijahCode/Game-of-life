95553af338cd9a1b0996fae9222748bb7695aae1
Ilya <ilyastarickov1996@gmail.com>
2021-04-10 13:42:51 +0400

    Install and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "",
+        })
+    ],
+    resolve: {
+        extensions: [".js", ".ts"],
+    },
+    rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        { 
+            test: /\.tsx?$/, 
+            loader: "ts-loader" 
+        }
+    ],
+};
